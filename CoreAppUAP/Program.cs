bdd39301ab433bc5a339bf9adbd0b71d9715c8ed
wherewhere <qq2518766683@outlook.com>
2025-03-08 21:24:47 +0800

    新建项目

---
+++ CoreAppUAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml;
+
+namespace CoreAppUAP
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Start(static p => _ = new App());
+        }
+    }
+}
